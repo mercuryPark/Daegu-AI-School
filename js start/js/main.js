2e016329d4b31d4d7566e7cc32c80aa2d706f504
mercuryPark <76655229+mercuryPark@users.noreply.github.com>
2021-07-26 14:33:49 +0900

    자바스크립트 1

---
+++ js start/js/main.js
@@ -0,0 +1,135 @@
+// comic 박스를 생성한 상태
+// 변수 선언
+// var comic;
+
+// comic 박스 안에 원피스 데이터를 기입한 상태
+// 변수 초기화
+// comic = "원피스";
+
+
+
+// console.log(comic);
+
+
+// var apple = "애플";
+
+// console.log("애플");
+
+
+// var = a;
+// var = b;
+// var = console;
+
+// var a, 
+//     b,
+//     c;
+
+//  변수 동시 선언
+
+// var a, b, c;
+
+//변수 동시 초기화
+
+// var d= 10;
+// var e= 20;
+// var f= 30;
+
+// var d = 10, e = 20, f= 30;
+
+
+//모든 변수는 선언과 동시에 undefined 자동으로 할당된다.
+// var computer;
+
+// console.log(computer);
+
+// var house;
+// house = "집";
+
+// console.log(house);
+
+// house = "아파트";
+
+// console.log(house);
+
+
+// var house;
+// console.log(house);
+
+// house = "집"
+// console.log(house);
+
+// 캐멀 케이스
+// var mathScoreStudent = 10;
+
+// 스네이크 케이스
+// var math_score_student = 10;
+
+// 주의점
+// var math-score-student = 100;
+
+// var _math;
+// var $math;
+
+//숫자가 문자보다 먼저나와선 안된다.
+// var apple10;
+// var 10apple;
+
+// 의미있게 작성해야된다.
+// var width_T
+
+
+// 데이터 타입 (데이터 종류 = 데이터 타입)
+// 두가지의 진형 
+// 문자열(string) 숫자(number) 논리(boolean) undefined  null = 원시 타입
+// 객체(object)  배열(array) 함수(function) = 참조 타입
+
+// 각각의 종류들마다 특성들이 존재한다.
+
+
+//데이터 타입
+
+var msg1 = ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
